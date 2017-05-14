6eaaafee4ec71eba1d18ccd673838450f23adf69
Joel George V <joel.gv3@gmail.com>
2017-05-14 20:50:14 +0530

    Get wifi stats using http request

--- index.js
+++ index.js
@@ -2,7 +2,37 @@
 
 'use strict';
 
+const http = require('http');
 const program = require('commander');
+
+const getStats = function () {
+
+    const options = {
+        host: 'onhub.here',
+        path: '/api/v1/status'
+    };
+
+    const callback = function (response) {
+        var str = '';
+
+        response.on('data', function (chunk) {
+            str += chunk;
+        });
+
+        response.on('end', function () {
+            const stats = JSON.parse(str);
+            console.log('Google Wifi Stats:', stats);
+        });
+    }
+
+    const request = http.request(options, callback);
+
+    request.on('error', function () {
+        console.log('Your Google Wifi is offline.');
+    });
+
+    request.end();
+}
 
 program
     .option('-v, --version', 'Software Version')
@@ -17,3 +47,5 @@ program
 if (!process.argv.slice(2).length) {
     program.help();
 }
+
+getStats();
