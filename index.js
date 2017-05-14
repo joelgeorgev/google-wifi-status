98a885bed6d1fde11599d7f44d955339160b0f2c
Joel George V <joel.gv3@gmail.com>
2017-05-14 20:34:28 +0530

    Add cli options using commander

--- index.js
+++ index.js
@@ -2,4 +2,18 @@
 
 'use strict';
 
-console.log('Hello World!');
+const program = require('commander');
+
+program
+    .option('-v, --version', 'Software Version')
+    .option('-u, --uptime', 'Total Uptime')
+    .option('-g, --gip', 'Gateway IP')
+    .option('-i, --ip', 'IP Address')
+    .option('-d, --dns', 'DNS Servers')
+    .option('-a, --all', 'Display all stats')
+    .parse(process.argv);
+
+// If no option passed, show help
+if (!process.argv.slice(2).length) {
+    program.help();
+}
