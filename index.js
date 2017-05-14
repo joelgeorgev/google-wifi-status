aa65b62d1e6f7021ce5907a0615f1e9f9b02ffc6
Joel George V <joel.gv3@gmail.com>
2017-05-14 21:03:47 +0530

    Display stats based on options passed

--- index.js
+++ index.js
@@ -21,7 +21,19 @@ const getStats = function () {
 
         response.on('end', function () {
             const stats = JSON.parse(str);
-            console.log('Google Wifi Stats:', stats);
+
+            if (program.swversion) { console.log('Software Version:', stats.software.softwareVersion); }
+            if (program.uptime) { console.log('Total Uptime:', stats.system.uptime); }
+            if (program.gip) { console.log('Gateway IP:', stats.wan.gatewayIpAddress); }
+            if (program.ip) { console.log('IP Address:', stats.wan.localIpAddress); }
+            if (program.dns) { console.log('DNS Servers:', stats.wan.nameServers); }
+            if (program.all) {
+                console.log('Software Version:', stats.software.softwareVersion);
+                console.log('Total Uptime:', stats.system.uptime);
+                console.log('Gateway IP:', stats.wan.gatewayIpAddress);
+                console.log('IP Address:', stats.wan.localIpAddress);
+                console.log('DNS Servers:', stats.wan.nameServers);
+            }
         });
     }
 
@@ -35,7 +47,7 @@ const getStats = function () {
 }
 
 program
-    .option('-v, --version', 'Software Version')
+    .option('-v, --swversion', 'Software Version')
     .option('-u, --uptime', 'Total Uptime')
     .option('-g, --gip', 'Gateway IP')
     .option('-i, --ip', 'IP Address')
