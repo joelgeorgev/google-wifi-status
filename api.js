03b305e2c92fe17c89ef5850ee62331ee1f29b37
Joel George V <joel.gv3@gmail.com>
2017-05-17 22:40:43 +0530

    Move fetch api code to api.js, use promise

---
+++ api.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const http = require('http');
+
+module.exports = new Promise((resolve, reject) => {
+
+	const options = {
+		host: 'onhub.here',
+		path: '/api/v1/status'
+	};
+
+	const request = http.request(options, response => {
+		let str = '';
+		response.on('data', (chunk => { str += chunk; }));
+		response.on('end', (() => { resolve(JSON.parse(str)); }));
+	});
+
+	request.on('error', (() => { reject('Your Google Wifi is offline.') }));
+	request.end();
+});
--- index.js
+++ index.js
@@ -2,77 +2,51 @@
 
 'use strict';
 
-const http = require('http');
 const program = require('commander');
 const chalk = require('chalk');
 const prettyMs = require('pretty-ms');
+const api = require('./api');
 const text = chalk.blue;
 const error = chalk.red;
 const log = console.log;
 
-const getStatus = function () {
-
-    const options = {
-        host: 'onhub.here',
-        path: '/api/v1/status'
-    };
-
-    const callback = function (response) {
-        let str = '';
-
-        response.on('data', function (chunk) {
-            str += chunk;
-        });
-
-        response.on('end', function () {
-            const status = JSON.parse(str);
-
-            if (program.swversion) { log(text('Software Version:', status.software.softwareVersion)); }
-            if (program.uptime) { log(text('Total Uptime:', prettyMs(status.system.uptime * 1000, { verbose: true }))); }
-            if (program.gip) { log(text('Gateway IP:', status.wan.gatewayIpAddress)); }
-            if (program.ip) { log(text('IP Address:', status.wan.localIpAddress)); }
-            if (program.dns) { log(text('DNS Servers:', status.wan.nameServers)); }
-            if (program.all) {
-                log(text('\nSoftware:\n'));
-                log(text('Software Version:', status.software.softwareVersion));
-                log(text('Update Channel:', status.software.updateChannel));
-                log(text('Latest Software Version:', status.software.updateNewVersion));
-                log(text('Update Required:', status.software.updateRequired));
-                log(text('\nSystem:\n'));
-                log(text('Country Code:', status.system.countryCode.toUpperCase()));
-                log(text('Device ID:', status.system.deviceId));
-                log(text('Hardware ID:', status.system.hardwareId));
-                log(text('Total Uptime:', prettyMs(status.system.uptime * 1000, { verbose: true })));
-                log(text('\nWAN:\n'));
-                log(text('Gateway IP:', status.wan.gatewayIpAddress));
-                log(text('IP Method:', status.wan.ipMethod.toUpperCase()));
-                log(text('IP Address:', status.wan.localIpAddress));
-                log(text('DNS Servers:', status.wan.nameServers));
-            }
-        });
-    }
-
-    const request = http.request(options, callback);
-
-    request.on('error', function () {
-        log(error('Your Google Wifi is offline.'));
-    });
-
-    request.end();
-}
-
 program
-    .option('-v, --swversion', 'Software Version')
-    .option('-u, --uptime', 'Total Uptime')
-    .option('-g, --gip', 'Gateway IP')
-    .option('-i, --ip', 'IP Address')
-    .option('-d, --dns', 'DNS Servers')
-    .option('-a, --all', 'All Stats')
-    .parse(process.argv);
+	.option('-v, --swversion', 'Software Version')
+	.option('-u, --uptime', 'Total Uptime')
+	.option('-g, --gip', 'Gateway IP')
+	.option('-i, --ip', 'IP Address')
+	.option('-d, --dns', 'DNS Servers')
+	.option('-a, --all', 'All Stats')
+	.parse(process.argv);
 
 // If no option passed, show help
 if (!process.argv.slice(2).length) {
-    program.help();
+	program.help();
 }
 
-getStatus();
+api
+	.then(status => {
+		if (program.swversion) { log(text('Software Version:', status.software.softwareVersion)); }
+		if (program.uptime) { log(text('Total Uptime:', prettyMs(status.system.uptime * 1000, { verbose: true }))); }
+		if (program.gip) { log(text('Gateway IP:', status.wan.gatewayIpAddress)); }
+		if (program.ip) { log(text('IP Address:', status.wan.localIpAddress)); }
+		if (program.dns) { log(text('DNS Servers:', status.wan.nameServers)); }
+		if (program.all) {
+			log(text('\nSoftware:\n'));
+			log(text('Software Version:', status.software.softwareVersion));
+			log(text('Update Channel:', status.software.updateChannel));
+			log(text('Latest Software Version:', status.software.updateNewVersion));
+			log(text('Update Required:', status.software.updateRequired));
+			log(text('\nSystem:\n'));
+			log(text('Country Code:', status.system.countryCode.toUpperCase()));
+			log(text('Device ID:', status.system.deviceId));
+			log(text('Hardware ID:', status.system.hardwareId));
+			log(text('Total Uptime:', prettyMs(status.system.uptime * 1000, { verbose: true })));
+			log(text('\nWAN:\n'));
+			log(text('Gateway IP:', status.wan.gatewayIpAddress));
+			log(text('IP Method:', status.wan.ipMethod.toUpperCase()));
+			log(text('IP Address:', status.wan.localIpAddress));
+			log(text('DNS Servers:', status.wan.nameServers));
+		}
+	})
+	.catch(err => { log(error(err)); });
